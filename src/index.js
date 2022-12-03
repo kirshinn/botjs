6ea9e5b6c3075b8d8666a064641732ceeafd702d
Kirshin Aleksandr <aleksandr.s.kirshin@gmail.com>
2022-12-04 01:27:23 +0700

    Final commit

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const TelegramApi = require('node-telegram-bot-api');
 const {gameOptions, againOptions} = require('./options');
 const sequilize = require('./db');
+const User = require('./models');
 
 const bot = new TelegramApi(process.env.TELEGRAM_API_TOKEN, {polling: true});
 
@@ -20,7 +21,6 @@ const startGame = async chatId => {
 }
 
 const start = async () => {
-
     try {
         await sequilize.authenticate();
         await sequilize.sync();
@@ -31,35 +31,41 @@ const start = async () => {
     bot.on('message', async msg => {
         const text = msg.text;
         const chatId = msg.chat.id;
-
-        if (text === '/start') {
-            return bot.sendMessage(chatId, `Добро пожаловать в telegram бот Kirshin Bot!`);
+        try {
+            if (text === '/start') {
+                const user = await User.findOne({chatId});
+                if (!user) await User.create({chatId});
+                return bot.sendMessage(chatId, `Добро пожаловать в telegram бот Kirshin Bot!`);
+            }
+            if (text === '/info') {
+                const user = await User.findOne({chatId});
+                await bot.sendMessage(chatId, `Вы вошли как ${msg.from.username}`);
+                return bot.sendMessage(chatId, `В игре у вас ${user.right} правильных ответов, ${user.wrong} неправильных ответов.`);
+            }
+            if (text === '/game') {
+                return startGame(chatId);
+            }
+            return bot.sendMessage(chatId, 'Данной команды не существует');
+        } catch (e) {
+            return bot.sendMessage(chatId, 'Произошла какая то ошибка!');
         }
-
-        if (text === '/info') {
-            return bot.sendMessage(chatId, `Вы вошли как ${msg.from.username}`);
-        }
-
-        if (text === '/game') {
-            return startGame(chatId);
-        }
-
-        return bot.sendMessage(chatId, 'Данной команды не существует');
     });
 
-    bot.on('callback_query', msg => {
+    bot.on('callback_query', async msg => {
         const data = msg.data;
         const chatId = msg.message.chat.id;
-
+        const user = await User.findOne({chatId});
         if (data === '/again') {
             return startGame(chatId);
         }
-
-        if(data === chats[chatId]) {
-            return bot.sendMessage(chatId, `Поздравляю, ты отгадал цифру: ${chats[chatId]}`, againOptions);
+        if (data === chats[chatId]) {
+            user.right++;
+            await bot.sendMessage(chatId, `Поздравляю, ты отгадал цифру: ${chats[chatId]}`, againOptions);
         } else {
-            return bot.sendMessage(chatId, `К сожалению ты не отгадал, бот загадал цифру: ${chats[chatId]}`, againOptions);
+            user.wrong++;
+            await bot.sendMessage(chatId, `К сожалению ты не отгадал, бот загадал цифру: ${chats[chatId]}`, againOptions);
         }
+        await user.save();
     });
 }
 
